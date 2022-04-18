82f20ed2151a7a580032f28776146bc25490a5d8
Joseph <softjoe40@gmail.com>
2022-04-18 01:41:34 +0100

    create navbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>My react first page</h1>
+      <div ></div>
+    </div>
+  );
+}
+
+export default App;
